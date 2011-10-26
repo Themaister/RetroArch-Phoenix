5dafd46b51bc68c702df5e3cdf779e4b5e606b96
Themaister <maister@archlinux.us>
2011-10-26 23:36:52 +0200

    Fix borked buttons on OSX.

--- phoenix/qt/widget/button.cpp
+++ phoenix/qt/widget/button.cpp
@@ -1,7 +1,11 @@
 Geometry pButton::minimumGeometry() {
   Font &font = this->font();
   Geometry geometry = font.geometry(button.state.text);
+#ifdef __APPLE__
+  return { 0, 0, geometry.width + 30, geometry.height + 12 };
+#else
   return { 0, 0, geometry.width + 20, geometry.height + 12 };
+#endif
 }
 
 void pButton::setText(const string &text) {
