b8f771b1efb5157de37d28bfa5487c03fdb34d9d
Themaister <maister@archlinux.us>
2012-06-03 21:54:44 +0200

    Allow cue sheets.

--- retroarch-phoenix.cpp
+++ retroarch-phoenix.cpp
@@ -865,7 +865,8 @@ class MainWindow : public Window
             // full list of pre-dreamcast sega rom file extensions: .bin, .gen, .md, .smd, .mdx, .sms, .gg, .sg
             // Genesis-next-libretro extensions are below.  Genplus-gx supports the full list above, so they should probably be added at some point.
             // Perhaps it would be prudent to add them now so that ROM load failure is up to the emulator core rather than this unzipping list.
-            ".bin", ".gen", ".md", ".smd", ".sms", ".gg"
+            ".bin", ".gen", ".md", ".smd", ".sms", ".gg",
+            ".cue",
          };
 
          foreach (file, z.file)
